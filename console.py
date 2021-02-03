ed3c138d0423d67e0bb6639ae801a1a962d5a09c
selsmie <simonelsmie@hotmail.com>
2021-02-03 16:10:26 +0000

    mvp completed

--- console.py
+++ console.py
@@ -10,6 +10,9 @@ author_repository.save(author_1)
 
 book_1 = Book("The Fellowship of the Rings", author_1, 1954)
 book_repository.save(book_1)
+
+print(book_1.id)
+
 book_2 = Book("The Two Towers", author_1, 1954)
 book_repository.save(book_2)
 book_3 = Book("The Return of the King", author_1, 1955)
--- repositories/book_repository.py
+++ repositories/book_repository.py
@@ -21,7 +21,7 @@ def select_all():
 
     for row in results:
         author = author_repository.select(row['author_id'])
-        book = Book(row['title'], author, row['year'])
+        book = Book(row['title'], author, row['year'], row['id'])
         books.append(book)
     return books
 
